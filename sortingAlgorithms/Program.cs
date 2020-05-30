745318694c297bdebcf9798ac0ceca4cdeb4c8d7
sai yadavalli <davinciharsha@gmail.com>
2020-05-30 23:09:49 +0530

    changes related to quick sort also

--- sortingAlgorithms/Program.cs
+++ sortingAlgorithms/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿ using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Concurrent;
 
 namespace sortingAlgorithms
 {
@@ -28,7 +30,7 @@ namespace sortingAlgorithms
 
             Pgmobject.printElements(arr);
 
-            Console.WriteLine("1 - bubble sort, 2 - selection sort");
+            Console.WriteLine("1 - bubble sort, 2 - selection sort, 3 - insertion sort, 4 - quick sort");
 
             int selectedSort = int.Parse(Console.ReadLine());
 
@@ -42,6 +44,16 @@ namespace sortingAlgorithms
                 case 2:
                     {
                         Pgmobject.selectionSort(arr);
+                        break;
+                    }
+                case 3:
+                    {
+                        Pgmobject.insertionSort(arr);
+                        break;
+                    }
+                case 4:
+                    {
+                        Pgmobject.quickSort(arr);
                         break;
                     }
                 default:
@@ -99,16 +111,16 @@ namespace sortingAlgorithms
         public void selectionSort(int[] arr)
         {
             int selSorLen = arr.Length;
-            for (int i = 0; i < arr.Length-1; i++)
+            for (int i = 0; i < arr.Length - 1; i++)
             {
-                for(int j=0;j<arr.Length-1;j++)
-                if (arr[j] > arr[j+1])
-                {
-                    int swapVar = 0;
-                    swapVar = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = swapVar;
-                }
+                for (int j = 0; j < arr.Length - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int swapVar = 0;
+                        swapVar = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = swapVar;
+                    }
 
                 foreach (int ival in arr)
                 {
@@ -138,10 +150,105 @@ namespace sortingAlgorithms
             Console.WriteLine();
         }
 
-        public void pushInToArray(int val,int pushArrLen)
+        public void insertionSort(int[] arr)
         {
-            int[] inProcessArray = new int[pushArrLen+1];
-            for (int i = pushArrLen; i < pushArrLen+1; i++)
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j-1] > arr[j])
+                    {
+                        {
+                            int swapVar = 0;
+                            swapVar = arr[j-1];
+                            arr[j-1] = arr[j];
+                            arr[j] = swapVar;
+                        }
+                    }
+                }
+                Console.WriteLine("after each i loop");
+                foreach (int ival in arr)
+                {
+                    Console.Write("{0} ", ival);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("After insertion sort");
+            foreach (int i in arr)
+            {
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+        }
+
+        public void quickSort(int[] arr)
+        {
+            Console.WriteLine("inside quick sort");
+            int LB = 0;
+            int UB = arr.Length - 1;
+            Console.WriteLine("before sending in begininning");
+            showArray(arr);
+            partition(arr, LB, UB);
+            Console.WriteLine("finally!!");
+            showArray(arr);
+        }
+
+        public void partition(int[] arr,int lb,int ub)
+        {
+            int pivot = lb;
+            int start = lb;
+            int end = ub;
+            while(start <= end)
+            {
+                while(arr[start] <= arr[pivot])
+                {
+                    start++;
+                }
+                while(arr[end] > arr[pivot])
+                {
+                    if(end>0)
+                    end--;
+                }
+                //swap start and end
+                if(start < end)
+                {
+                    int temp = 0;
+                    temp = arr[start];
+                    arr[start] = arr[end];
+                    arr[end] = temp;
+                }
+            }
+            //swap a[lb] and pivot
+            int tempv = 0;
+            tempv = arr[lb];
+            arr[lb] = arr[end];
+            arr[end] = tempv;
+
+            partition(arr, start, end - 1);
+            Console.WriteLine("first half");
+            showArray(arr);
+            partition(arr, end+1, arr.Length-1);
+            Console.WriteLine("second half");
+            showArray(arr);
+        }
+
+        //public void swap(int s, int e)
+        //{
+        //
+        //}
+
+        public void showArray(int[] arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+
+        public void pushInToArray(int val, int pushArrLen)
+        {
+            int[] inProcessArray = new int[pushArrLen + 1];
+            for (int i = pushArrLen; i < pushArrLen + 1; i++)
             {
                 inProcessArray[i] = val;
             }
